74bd572a4f523120b3fb3260ae982b1696968811
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-10-23 00:17:53 -0500

    one to many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Satya", last_name: "Boggula", hourly_rate: 30)
+
+@store1.employees.create(first_name: "Divya", last_name: "Vemireddy", hourly_rate: 40)
+
+@store2.employees.create(first_name: "Usha", last_name: "Veluri", hourly_rate: 50)
+
+@store2.employees.create(first_name: "Ram", last_name: "Veluri", hourly_rate: 55)
+
+@store2.employees.create(first_name: "Krishna", last_name: "Vema", hourly_rate: 35)
