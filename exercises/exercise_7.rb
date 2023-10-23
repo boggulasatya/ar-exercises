eadc13de8c5d2a617effba08bf1a093045061d9c
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-10-23 00:36:04 -0500

    validations for store and employee models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,19 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+#  Ask the user for a store name
+print "Enter a store name: "
+store_name = gets.chomp
+
+# Attempt to create a store with the inputted name
+new_store = Store.new(name: store_name)
+
+# Attempt to save/create the record and display error messages
+if new_store.save
+  puts "Store successfully created!"
+else
+  puts "Failed to create the store. Errors:"
+  new_store.errors.full_messages.each do |error|
+    puts error
+  end
+end
