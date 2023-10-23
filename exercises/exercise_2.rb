3cad7eb90288550d423898c33981e283aed3f79a
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-10-22 23:23:30 -0500

    update first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+
+# Load the second store from the database and assign it to `@store2`.
+
+@store2 = Store.find(2)
+
+# Update the first store (`@store1`) instance in the database. (Change its name or something.)
+
+@store1.update(name: 'Winnipeg')
